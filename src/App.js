78b4b0b080520820dd9bacdec44f60b55ae2ed0e
Paulo Silveira <paulossnjf@gmail.com>
2021-02-07 21:13:15 -0300

    Footer finalizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 // Importando o CSS gerado pelo SASS com todo
@@ -9,14 +10,18 @@ import "./CSS/style.min.css";
 function App() {
   return (
     <section>
+      {/* Header principal do site */}
       <Header />
       <Router>
         <Switch>
+          {/* Home do site */}
           <Route path="/">
             <Home />
           </Route>
         </Switch>
       </Router>
+      {/* Rodapé do site */}
+      <Footer />
     </section>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+// Importando o logo da pixem
+import logoPixem from "../../Assets/logoPixemLight.png";
+// Importando os ícones do github
+import { ReactComponent as GithubIcon } from "../../Assets/github.svg";
+import { ReactComponent as GithubWhiteIcon } from "../../Assets/githubWhite.svg";
+
+const Footer = () => {
+  // Tema atual do site
+  const { theme } = useSelector((state) => state.userInterface);
+
+  return (
+    <footer className={`footer`}>
+      <div className="container">
+        <div className="footerWrapper">
+          <div className="collumn logo">
+            <Router>
+              <Link to="/">
+                <img src={logoPixem} alt="Logo da Pixem Stock" />
+              </Link>
+            </Router>
+          </div>
+          <div className="collumn">
+            <h2 className="subtitle"> Pexels </h2>
+            <div className="text">
+              <a
+                href="https://www.pexels.com"
+                target="_blank"
+                rel="noreferrer"
+                title="Pexels"
+              >
+                Fotos fornecidas pela Pexels
+              </a>
+              <img
+                src="https://images.pexels.com/lib/api/pexels-white.png"
+                alt="Logo da Pexels"
+              />
+            </div>
+          </div>
+          <div className="collumn">
+            <h2 className="subtitle"> Flaticon </h2>
+            <div className="text">
+              <div>
+                Icones feitos pela{" "}
+                <a
+                  href="https://www.flaticon.com/authors/pixel-perfect"
+                  title="Pixel perfect"
+                >
+                  {" "}
+                  Pixel perfect{" "}
+                </a>
+                da
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                  {" "}
+                  www.flaticon.com
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`miniFooter ${theme ? "darkTheme" : "lightTheme"}`}>
+        <p>
+          <a
+            href="https://github.com/Paulo-ss"
+            target="_blank"
+            rel="noreferrer"
+            className={`${theme ? "lightColor" : "darkColor"}`}
+          >
+            {" "}
+            Site desenvolvido por Paulo_ss{" "}
+            {theme ? <GithubWhiteIcon /> : <GithubIcon />}
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
