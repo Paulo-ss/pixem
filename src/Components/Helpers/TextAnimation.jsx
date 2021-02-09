c87300a9e0d4368347cf3ead8a53c7c835b45177
Paulo Silveira <paulossnjf@gmail.com>
2021-02-09 12:50:51 -0300

    Animação do texto sendo escrito na tela adicionado

---
+++ src/Components/Helpers/TextAnimation.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const TextAnimation = ({ children }) => {
+  // Ref para o elemento que tem o texto
+  // a ser animado
+  const element = React.useRef(null);
+
+  // Animação do texto sendo escrito na tela
+  React.useLayoutEffect(() => {
+    // Selecionando o elemento de texto dentro do
+    // elemento pai no ref para acessar o texto
+    const textElement = element.current.children[0];
+    const letras = textElement.innerHTML.split("");
+    textElement.innerText = "";
+
+    // Animando a entrada de cada letra
+    let timeout;
+    letras.forEach((letra, index) => {
+      timeout = setTimeout(() => {
+        textElement.innerText += letra;
+      }, index * 60);
+    });
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <div ref={element} className={`textAnimation`}>
+      {children}
+    </div>
+  );
+};
+
+export default TextAnimation;
