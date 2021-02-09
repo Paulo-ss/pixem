e67a8e7b4e27e86101b199fb13d7f7f265dc8d90
Paulo Silveira <paulossnjf@gmail.com>
2021-02-09 15:28:55 -0300

    Animações de entrada ao scroll da página adicionadas

--- src/Components/Home/PalavrasChave.jsx
+++ src/Components/Home/PalavrasChave.jsx
@@ -6,8 +6,40 @@ const PalavrasChave = () => {
   // Acessando o tema atual do site
   const { theme } = useSelector((state) => state.userInterface);
 
+  // Ref para o elemento com animação de entrada
+  const element = React.useRef(null);
+
+  // Animando a seção quando ela entrar na tela
+  React.useLayoutEffect(() => {
+    // Valor de 60% da tela do usuário
+    const windowSize = window.innerHeight * 0.6;
+
+    // Distância do topo da página do elemento
+    // no ref
+    const elementTopPosition = element.current.offsetTop - windowSize;
+
+    // Função que lida com o scroll
+    const onScroll = () => {
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition > elementTopPosition) {
+        element.current.classList.add("animateEntry");
+      }
+    };
+
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
-    <section className={`palavrasChave ${theme ? "darkTheme" : "lightTheme"}`}>
+    <section
+      ref={element}
+      className={`palavrasChave ${theme ? "darkTheme" : "lightTheme"}`}
+    >
       <div className="container">
         <div className="title">
           <h1> Palavras chave para a busca </h1>
